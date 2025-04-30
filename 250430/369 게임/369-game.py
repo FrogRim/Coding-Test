b9770a8a860b1c383430ce59d33b543179cce6c4
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-04-30 15:09:27 +0900

    Title: 369 게임, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250430/369 게임/369-game.py
+++ 250430/369 게임/369-game.py
@@ -1,8 +1,9 @@
 N = int(input())
 
 for i in range(1,N+1):
-    if i % 3==0 or  i / 10 == 3 or i / 10 == 6 or i / 10 == 9 or i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+    if ((i % 3==0) or (i // 10 == 3) or (i // 10 == 6) or (i // 10 == 9) or (i % 10 == 3) or (i % 10 == 6) or (i % 10 == 9)):
         print(0,end=' ')
+        
     else:
         print(i, end=' ')
      
