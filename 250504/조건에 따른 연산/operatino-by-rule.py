c8919a2efc77c79e86ac0225030950661d6c9224
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-05-04 23:22:14 +0900

    Title: 조건에 따른 연산, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250504/조건에 따른 연산/operatino-by-rule.py
+++ 250504/조건에 따른 연산/operatino-by-rule.py
@@ -2,12 +2,16 @@ cnt = 0
 n = int(input())
 while n < 1000:
     if n % 2 == 0:
-        n = 2 * n + 2
-        if n >= 1000:
-            break
-    else:
         n = 3 * n + 1
-        if n >= 1000:
+        cnt += 1
+        if n > 1000:
             break
-    cnt += 1
+        
+    else:
+        n = 2 * n + 2
+        cnt += 1
+        if n > 1000:
+            break
+        
+    
 print(cnt)
