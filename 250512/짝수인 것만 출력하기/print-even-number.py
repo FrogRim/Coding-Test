ba112bacf50ee230eb86f09f6af01864ce2c4d49
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-05-12 23:23:15 +0900

    Title: 짝수인 것만 출력하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250512/짝수인 것만 출력하기/print-even-number.py
+++ 250512/짝수인 것만 출력하기/print-even-number.py
@@ -1,6 +1,6 @@
 n = int(input())
 
-list_ = [x for x in input().split() if x % 2 == 0]
+list_ = [int(x) for x in input().split() if int(x) % 2 == 0]
 
-for i in range(n):
-    print(list_[i] ,end=" ")
+for i in list_:
+    print(i,end=" ")
