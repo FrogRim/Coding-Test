389cf4edbde2efff6d944a192000d90a1fd30f1d
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-05-22 23:17:00 +0900

    Title: 격자에 점 그리기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250522/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,14 @@
+n,m = map(int,input().split())
+
+placed = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+cnt = 1
+for _ in range(m):
+    r, c = tuple(map(int, input().split()))
+    placed[r-1][c-1] = cnt
+    cnt += 1
+
+for row in placed:
+    print(*row)
