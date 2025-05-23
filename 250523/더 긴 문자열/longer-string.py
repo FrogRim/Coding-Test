df14d0ceb850140e07119930b791354d92ffee75
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-05-23 16:02:15 +0900

    Title: 더 긴 문자열, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250523/더 긴 문자열/longer-string.py
+++ 250523/더 긴 문자열/longer-string.py
@@ -2,7 +2,7 @@ given_input,given_input1 = input().split()
 
 if len(given_input) > len(given_input1):
     print(f"{given_input} {len(given_input)}")
-if len(given_input) < len(given_input1):
+elif len(given_input) < len(given_input1):
     print(f"{given_input1} {len(given_input1)}")
 else:
     print("same")
