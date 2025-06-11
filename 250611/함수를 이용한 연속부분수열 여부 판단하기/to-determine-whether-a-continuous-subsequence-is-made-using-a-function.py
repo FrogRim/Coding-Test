a532b619da7558883800cf5d5c1c040b27186ae4
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-06-11 15:31:14 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250611/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 250611/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -4,11 +4,12 @@ b = list(map(int, input().split()))
 
 # Please write your code here.
 def check(a,b):
-    max_lst = a if len(a) > len(b) else b
-    min_lst = b if len(a) > len(b) else a
+    max_lst = a 
+    min_lst = b 
 
     for i in range(len(max_lst)):
         temp = max_lst[i:i+len(min_lst)]
+        
         if temp == min_lst:
             return True
     
