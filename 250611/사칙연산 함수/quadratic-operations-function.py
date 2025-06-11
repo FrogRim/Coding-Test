f5fd881a3b400a94d27290d9b90b06e37624d70e
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-06-11 14:52:58 +0900

    Title: 사칙연산 함수, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250611/사칙연산 함수/quadratic-operations-function.py
+++ 250611/사칙연산 함수/quadratic-operations-function.py
@@ -5,6 +5,7 @@ c = int(c)
 # Please write your code here.
 o = str(o)
 
+# python은 switch문이 없어 이렇게 dict와 get을 이용해 switch기능 구현
 def switch(a,o,c):
   result = {"+" : a + c, "-": a - c, "/" : a // c, "*" : a * c}.get(o, "False")
   return result 
