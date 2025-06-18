65b80a3288a96f405dc22a575aafcc4839d7d47f
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-06-18 18:38:10 +0900

    Title: 1부터 특정 수까지의 합 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250618/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int Fact(int n) {
+    if(n == 1)
+        return 1;
+
+    return Fact(n - 1) + n;
+}
+
+int main() {
+    cin >> N;
+    cout << Fact(N);
+    return 0;
+}
