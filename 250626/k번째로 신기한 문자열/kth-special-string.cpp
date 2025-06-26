448791cc604ff6e19e920dd35d19ed1a20cfa55c
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-06-26 15:37:36 +0900

    Title: k번째로 신기한 문자열, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250626/k번째로 신기한 문자열/kth-special-string.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n, k;
+string t;
+string str[100];
+vector<string> tmp;
+
+
+
+int main() {
+    cin >> n >> k >> t;
+
+    for (int i = 0; i < n; i++) {
+        
+        cin >> str[i];
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+      
+
+        if (str[i].find(t) != std::string::npos) {
+            tmp.push_back(str[i]);
+        }
+        
+    }
+    // Please write your code here.
+    sort(tmp.begin(), tmp.end());
+
+    if (k - 1 < tmp.size()) {
+        cout << tmp[k - 1] << '\n';
+    }
+
+    return 0;
+}
