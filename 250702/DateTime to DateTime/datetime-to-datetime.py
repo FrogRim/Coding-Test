2cf3d2742470254be9c6c70a43e25cbd6ddfcd50
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-02 12:47:53 +0900

    Title: DateTime to DateTime, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250702/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,29 @@
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+check = 0
+
+d = 11
+h = 11
+m = 11
+
+if b < 11 or (b == 11 and c < 11):
+    print(-1)
+
+else:
+    while True:
+        if d == a and h == b and m == c:
+            print(check)
+            break
+
+        m += 1
+        check +=1
+
+        if m == 60:
+            h += 1
+            m = 0
+
+        if h == 24:
+            d +=1
+            h = 0 
+
