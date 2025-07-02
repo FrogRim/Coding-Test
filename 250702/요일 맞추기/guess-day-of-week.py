0869e4ac4db94022cc8b3d34f7044237efc00216
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-02 15:19:49 +0900

    Title: 요일 맞추기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250702/요일 맞추기/guess-day-of-week.py
+++ 250702/요일 맞추기/guess-day-of-week.py
@@ -8,12 +8,26 @@ num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 k = 0
 
-w_l = num_of_days[m1:m2]
+if m1 <= m2:
+    w_l = num_of_days[m1:2]
+    for i in w_l:
+        k += i
+    #print(k)
 
-for i in w_l:
-    k += i
+    check = k + (d2 - d1)
 
-check = k + (d2 - d1)
+    print(day_week[check % 7]) 
+
+else:
+    w_l = num_of_days[m2:m1]
+    for i in w_l:
+        k += i
+    #print(k)
+
+    check = -(k + (d2 - d1))
+
+    #print(check % 7)
+
+    print(day_week[check % 7+2]) 
 
 
-print(day_week[check % 7])
