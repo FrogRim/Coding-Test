c7f2427bf0dcc7dc507eece307c7ea2750446d16
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-03 16:34:02 +0900

    Title: 요일 맞추기, Time: 1099ms, Memory: 16MB, Status: Time Limit Exceed - Codetree

---
+++ 250703/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,35 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+
+month, day = m1, d1
+elapsed_days = 0
+
+# Please write your code here.
+day_week = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+
+
+#                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+while True:
+    if month == m2 and day == d2:
+        break
+
+    if m2 < month or day > d2:
+        elapsed_days -= 1
+        day -= 1
+
+        if day < 1:
+            month -= 1
+            day = num_of_days[month]
+    
+    else:
+        elapsed_days += 1
+        day += 1
+
+        if day > num_of_days[month]:
+            month += 1
+            day = 1
+
+print(day_week[elapsed_days])
+#print(elapsed_days % 7)
