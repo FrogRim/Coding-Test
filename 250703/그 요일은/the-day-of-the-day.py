178c1e012d4baff44772eb5358297d7b284e8bf1
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-03 16:41:32 +0900

    Title: 그 요일은, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250703/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,20 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+# Please write your code here.
+day_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+# 날짜를 일 수로 변환하는 함수
+def days_since_start(month, day):
+    return sum(num_of_days[:month]) + day
+
+# 경과 일 수 계산
+start_days = days_since_start(m1, d1)
+end_days = days_since_start(m2, d2)
+elapsed_days = end_days - start_days
+
+result = day_week.index(A)
+
+# 요일 출력
+print((elapsed_days // int(result)) - 1)
