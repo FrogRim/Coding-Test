410a1a58feedf9f32739e810f1116dd58ebd1250
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-03 17:50:19 +0900

    Title: 그 요일은, Time: 54ms, Memory: 16MB, Status: Passed - Codetree

--- 250703/그 요일은/the-day-of-the-day.py
+++ 250703/그 요일은/the-day-of-the-day.py
@@ -2,7 +2,7 @@ m1, d1, m2, d2 = map(int, input().split())
 A = input()
 
 # Please write your code here.
-day_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+day_week = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
 num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 # 날짜를 일 수로 변환하는 함수
@@ -15,6 +15,13 @@ end_days = days_since_start(m2, d2)
 elapsed_days = end_days - start_days
 
 result = day_week.index(A)
-
+count = 0
 # 요일 출력
-print((elapsed_days // int(result)) - 1)
+# print((elapsed_days // int(result)))
+# print((elapsed_days % int(result)))
+
+for i in range(elapsed_days+1):
+    if day_week[i % 7] == A:
+        count +=1
+
+print(count)
