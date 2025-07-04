a9cfb4a7603d53bdbf3e456a34d41cb22ce3730f
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-04 16:08:46 +0900

    Title: 블럭쌓는 명령2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250704/블럭쌓는 명령2/block-stacking-commands2.py
+++ 250704/블럭쌓는 명령2/block-stacking-commands2.py
@@ -1,7 +1,7 @@
 n, k = map(int, input().split())
 commands = [tuple(map(int, input().split())) for _ in range(k)]
 
-blocks = [0 for _ in range(n)]
+blocks = [0 for _ in range(n+1)]
 
 # Please write your code here.
 for i in range(k):
