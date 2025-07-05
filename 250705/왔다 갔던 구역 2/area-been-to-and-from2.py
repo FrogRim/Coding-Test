f4b9003d9e2a02795d58494aeeeb7567ae51108e
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-05 18:36:09 +0900

    Title: 왔다 갔던 구역 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250705/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250705/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,26 +1,24 @@
 from collections import defaultdict
 
 n = int(input())
-x = []
-dir = []
-for _ in range(n):
-    xi, di = input().split()
-    x.append(int(xi))
-    dir.append(di)
+moves = [(int(x), d) for x, d in (input().split() for _ in range(n))]
 
-visited = defaultdict(int)
+visited_edges = defaultdict(int)
 cur_pos = 0
 
-for steps, direction in zip(x, dir):
+for steps, direction in moves:
     if direction == 'R':
-        for i in range(cur_pos, cur_pos + steps,1):
-            visited[i] += 1
+        for i in range(steps):
+            # 간선 식별: (작은 위치, 큰 위치)
+            edge = (cur_pos + i, cur_pos + i + 1)
+            visited_edges[edge] += 1
         cur_pos += steps
-    elif direction == 'L':
-        for i in range(cur_pos , cur_pos - steps, -1):
-            visited[i] += 1
+    else:  # 'L'
+        for i in range(steps):
+            edge = (cur_pos - i - 1, cur_pos - i)
+            visited_edges[edge] += 1
         cur_pos -= steps
 
-# Count positions visited more than once
-result = sum(1 for v in visited.values() if v >= 2)
+# 두 번 이상 지난 간선 수
+result = sum(1 for cnt in visited_edges.values() if cnt >= 2)
 print(result)
