92a0445854f42ae789a06161b61a7bbf9c6f1b38
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-05 18:11:09 +0900

    Title: 왔다 갔던 구역 2, Time: 55ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250705/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250705/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -13,14 +13,14 @@ cur_pos = 0
 
 for steps, direction in zip(x, dir):
     if direction == 'R':
-        for i in range(cur_pos, cur_pos + steps):
+        for i in range(cur_pos, cur_pos + steps,1):
             visited[i] += 1
         cur_pos += steps
     elif direction == 'L':
-        for i in range(cur_pos, cur_pos - steps, -1):
+        for i in range(cur_pos , cur_pos - steps, -1):
             visited[i] += 1
         cur_pos -= steps
 
 # Count positions visited more than once
-result = sum(1 for v in visited.values() if v > 1)
+result = sum(1 for v in visited.values() if v >= 2)
 print(result)
