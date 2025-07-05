3730ac09ce03f43d32ea799277e996f8f3fa760f
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-05 18:04:12 +0900

    Title: 왔다 갔던 구역 2, Time: 56ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250705/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250705/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 n = int(input())
 x = []
 dir = []
@@ -6,33 +8,19 @@ for _ in range(n):
     x.append(int(xi))
     dir.append(di)
 
-# Please write your code here.
-
-class Pos:
-    def __init__(self,pos,num):
-        self.pos = pos
-        self.num = num
-
-space = [Pos(i,0) for i in range(-1000,1001)]
-
+visited = defaultdict(int)
 cur_pos = 0
 
-for order1,order2 in zip(x,dir):
-    
-    if order2 == 'R':
-        for i in range(cur_pos,cur_pos+int(order1)+1):
-            space[1000+i].num += 1
-            cur_pos += order1
-        
-    elif order2 == 'L':
-        for i in range(cur_pos,(cur_pos-int(order1)-1),-1):
-            space[1000+i].num += 1
-            cur_pos -= order1
-        
-result = 0
-for t in space:
-    if 1 < t.num:
-        print(t.pos)
-        result += 1
+for steps, direction in zip(x, dir):
+    if direction == 'R':
+        for i in range(cur_pos, cur_pos + steps):
+            visited[i] += 1
+        cur_pos += steps
+    elif direction == 'L':
+        for i in range(cur_pos, cur_pos - steps, -1):
+            visited[i] += 1
+        cur_pos -= steps
 
-print(result)   
+# Count positions visited more than once
+result = sum(1 for v in visited.values() if v > 1)
+print(result)
