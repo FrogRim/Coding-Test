bc3e8541972f4f5822e80705c16641cbe6da67e9
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-05 16:54:36 +0900

    Title: 최대로 겹치는 구간, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250705/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -0,0 +1,29 @@
+n = int(input())
+segments = [tuple(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+class Point:
+    def __init__(self,positon,num):
+        self.positon = positon
+        self.num = num
+
+
+    
+space = [Point(i,0) for i in range(-100,101)]
+
+for s in segments:
+    for k in range(s[0],s[1]+1):
+
+        space[100 + k].num += 1
+
+result = 0
+for t in space:
+    if result < t.num:
+        result = t.num
+
+
+
+
+print(result)
+
