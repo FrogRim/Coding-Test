4d485c5a5c62f4252d8722e0e09d53584072ded6
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-05 16:57:10 +0900

    Title: 최대로 겹치는 지점, Time: 79ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250705/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -0,0 +1,26 @@
+n = int(input())
+segments = [tuple(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+
+class Point:
+    def __init__(self,positon,num):
+        self.positon = positon
+        self.num = num
+
+
+    
+space = [Point(i,0) for i in range(1,101)]
+
+for s in segments:
+    for k in range(s[0],s[1]+1):
+
+        space[k].num += 1
+
+result = 0
+for t in space:
+    if result < t.num:
+        result = t.num
+
+print(result)
