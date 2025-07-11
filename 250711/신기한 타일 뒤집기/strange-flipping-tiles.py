a186a7941fdb7ae7cf56255911fff3596e3605b3
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-11 15:11:22 +0900

    Title: 신기한 타일 뒤집기, Time: 100ms, Memory: 19MB, Status: Wrong Answer - Codetree

---
+++ 250711/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,62 @@
+n = int(input())
+commands = [tuple(input().split()) for _ in range(n)]
+x = []
+dir = []
+for num, direction in commands:
+    x.append(int(num))
+    dir.append(direction)
+
+# Please write your code here.
+class Point:
+    def __init__(self):
+        self.cnt_w = 0
+        self.cnt_b = 0
+        self.state = 'gray'
+
+    def paint_white(self):
+        self.cnt_w += 1
+        self._update_state("W")
+
+    def paint_black(self):
+        self.cnt_b += 1
+        self._update_state("B")
+
+    def _update_state(self, painted):
+        self.state = painted
+
+space = {}
+current_pos = 0
+
+for distance_str, direction in zip(x,dir):
+    distance = int(distance_str)
+
+    # 이동 방향과 범위 설정 (for문 안으로)
+    if direction == 'L':
+        start, end = current_pos - distance, current_pos 
+  
+        paint_method = 'white'
+    else:  # direction == 'R'
+        start, end = current_pos , current_pos + distance
+        
+        paint_method = 'black'
+
+    # 색칠하기
+    for pos in range(start, end):
+        if pos not in space:
+            space[pos] = Point()
+
+        if paint_method == 'white':
+            space[pos].paint_white()
+        else:
+            space[pos].paint_black()
+
+    # 현재 위치 업데이트
+    if direction == 'L':
+        current_pos -= distance
+    else:
+        current_pos += distance
+
+count_white = sum(1 for point in space.values() if point.state == 'W')
+count_black = sum(1 for point in space.values() if point.state == 'B')
+
+print(count_white, count_black)
