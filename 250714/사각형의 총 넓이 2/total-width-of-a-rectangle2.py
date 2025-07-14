b1b3e629aa89dd9b24ea7a9b56e76113cde49927
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-14 16:13:30 +0900

    Title: 사각형의 총 넓이 2, Time: 63ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250714/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -0,0 +1,30 @@
+n = int(input())
+x1, y1, x2, y2 = [], [], [], []
+for _ in range(n):
+    a, b, c, d = map(int, input().split())
+    x1.append(a)
+    y1.append(b)
+    x2.append(c)
+    y2.append(d)
+
+# Please write your code here.
+offset = 100
+
+board = [[ 0 for _ in range(offset * 2)] for _ in range(offset * 2)]
+
+
+for i in range(n):
+    x_min = min(x1[i]+offset,x2[i]+offset)
+    y_min = min(y1[i]+offset,y2[i]+offset)
+
+    x_max = max(x1[i]+offset,x2[i]+offset)  
+    y_max = max(y1[i]+offset,y2[i]+offset) 
+
+    for y in range(y_min,y_max + 1):
+        for x in range(x_min,x_max):
+            board[y][x] += 1
+           
+            
+            
+
+print(sum(1 for b in board for c in b if c == 1))
