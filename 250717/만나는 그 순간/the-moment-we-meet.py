355bfac752002509ae6ddb5b7faf6b22cdaa1bf0
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-17 14:47:27 +0900

    Title: 만나는 그 순간, Time: 105ms, Memory: 28MB, Status: Passed - Codetree

--- 250717/만나는 그 순간/the-moment-we-meet.py
+++ 250717/만나는 그 순간/the-moment-we-meet.py
@@ -2,7 +2,7 @@ def process_commands(directions, times):
     positions = [0]
     for dir, t in zip(directions, times):
         step = 1 if dir == 'R' else -1
-        for i in range(t):
+        for _ in range(t):
             positions.append(positions[-1] + step)
     return positions
 
@@ -25,12 +25,14 @@ for _ in range(m):
 positions_A = process_commands(d, t)
 positions_B = process_commands(d2, t2)
 
+positions_A.pop(0)  # 0 위치 제거
+positions_B.pop(0)
+
 result = -1
 
-for i,val_a in enumerate(positions_A):
-    for j,val_b in enumerate(positions_B):
-        if i == j and val_a == val_b:
-            result = val_a + 1
-        
+for i, (val_a, val_b) in enumerate(zip(positions_A, positions_B)):
+    if val_a == val_b:
+        result = i + 1  
+        break
 
 print(result)
