d8ea8e8c00143fc987115f63471c040b482dd904
FrogRim <127326495+FrogRim@users.noreply.github.com>
2025-07-23 12:07:34 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250723/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 250723/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -22,6 +22,7 @@ handshakes.sort(lambda x: x[0]) # 시간대 빠른 순으로 정렬
 
 for (_,x,y) in handshakes:
 
+   
     x_devloper = devleopers[x-1]
     y_devloper = devleopers[y-1]
     
@@ -29,11 +30,21 @@ for (_,x,y) in handshakes:
     if x_devloper.check == False and y_devloper.check == False:
         continue
     
+    #둘다 감염자이면 전염 횟수만 증가
+    elif x_devloper.check == True and y_devloper.check == True:
+        x_devloper.num_handshake += 1
+        y_devloper.num_handshake += 1
+        # print(f" 3:{x_devloper.num_handshake}")
+        # print(f" 3:{y_devloper.num_handshake}")
+
+    
     # 둘 중 하나라도 감염자면
     elif x_devloper.check == True:
         if x_devloper.num_handshake < K:
             y_devloper.check = True
             x_devloper.num_handshake += 1
+            # print(f"1:{x_devloper.num_handshake}")
+            # print(f"1:{y_devloper.num_handshake}")
         
 
     
@@ -41,11 +52,8 @@ for (_,x,y) in handshakes:
         if y_devloper.num_handshake < K:
             x_devloper.check = True
             y_devloper.num_handshake += 1
-
-    #둘다 감염자이면 전염 횟수만 증가
-    else:
-        x_devloper.num_handshake += 1
-        y_devloper.num_handshake += 1
+            # print(f" 2:{x_devloper.num_handshake}")
+            # print(f" 2:{y_devloper.num_handshake}")
 
 
 for i in devleopers:
